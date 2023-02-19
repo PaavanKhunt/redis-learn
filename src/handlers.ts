97c6e7c46ad59503f606f71b3ca73570fc649850
PaavanKhunt <khuntpaavan2001@gmail.com>
2023-02-20 00:01:23 +0530

    redis poc

---
+++ src/handlers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+interface HelloResponse {
+  hello: string;
+}
+
+type HelloBuilder = (name: string) => HelloResponse;
+
+const helloBuilder: HelloBuilder = (name) => ({ hello: name });
+
+export const rootHandler = (_req: Request, res: any) => {
+  return res.send('API is running fine 3! 🤓');
+};
+
+export const helloHandler = (req: Request, res: Response) => {
+  const { params } = req;
+  const { name = 'World' } = params;
+  const response = helloBuilder(name);
+
+  return res.json(response);
+};
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import axios from 'axios';
+import { createClient } from 'redis';
+import { rootHandler, helloHandler } from './handlers';
+
+const redisClient = createClient();
+
+redisClient.connect();
+
+console.log('Starting server...');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', rootHandler);
+app.get('/hello/:name', helloHandler);
+
+app.get('/photos', async (req, res) => {
+  const response = await redisClient.get('photos');
+  const photos = response ? JSON.parse(response) : null;
+
+  if (!!photos.length) {
+    console.log('Found in cache');
+    if (response) return res.json(JSON.parse(response));
+  } else {
+    console.log('Not found in cache');
+
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos`
+    );
+    await redisClient.set('photos', JSON.stringify(data));
+    res.json(data);
+  }
+});
+
+app.get('/photos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/photos/${id}`
+  );
+  res.json(data);
+});
+
+// function getOrSetCache(key: string, getter: () => void) {
+//   return new Promise((resolve, reject) => {
+//     redisClient.get(key, async (err: any, response: string) => {
+//       if (err) {
+//         reject(err);
+//       } else if (response) {
+//         resolve(JSON.parse(response));
+//       } else {
+//         const freshData = getter();
+//         redisClient.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(freshData));
+//         resolve(freshData);
+//       }
+//     });
+//   });
+// }
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`);
+});
